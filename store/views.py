f0295f0a6a078147ce19ca1330730990003297b3
Jose Tafur <j-osea-nt@hotmail.com>
2019-12-27 14:12:38 -0500

    adding the files

--- store/views.py
+++ store/views.py
@@ -3,5 +3,23 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html', {
-        
+        'message': 'Listado de productos',
+        'title': 'Productos',
+        'products': [
+            {
+                'title': 'Playera',
+                'price': 5,
+                'stock': True
+            },
+            {
+                'title': 'Polo',
+                'price': 8,
+                'stock': True
+            },
+            {
+                'title': 'Raton',
+                'price': 25,
+                'stock': True
+            }
+        ]
     })
