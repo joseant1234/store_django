bb310cbff286ee9e3fdac6e2e770f10d5b048d2d
Jose Tafur <j-osea-nt@hotmail.com>
2019-12-28 08:44:00 -0500

    adding the authentication

--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django # se renombra la funcion pues ya esta definido en la vista la funcion login
+from django.shortcuts import redirect
 
 def index(request):
     return render(request, 'index.html', {
@@ -23,3 +26,19 @@ def index(request):
             }
         ]
     })
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # si existe el usuario, muestra el user sino envia None a la variable
+        user = authenticate(username=username, password=password)
+
+        if user:
+            # se va a generar sesion. Requiera la peticion y el usuario
+            login_django(request, user)
+            # recibe como argunmento el nombre de la ruta
+            return redirect('index')
+
+    return render(request, 'users/login.html', {})
