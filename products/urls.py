c87b9bd6ab80b081be98d3d8ef293ec514e51dd3
Jose Tafur <j-osea-nt@hotmail.com>
2020-04-26 16:14:37 -0500

    adding app_name in product routes

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+# con app_name se esta definiendo q las rutas son parte de la aplicación
+app_name = 'products'
+
 # <PK> indica q el parametro es el # ID
 urlpatterns = [
     path('search', views.ProductSearchListView.as_view(), name='search'),
-    path('<slug:slug>', views.ProductDetailView.as_view(), name='product'),    
+    path('<slug:slug>', views.ProductDetailView.as_view(), name='product'),
 ]
