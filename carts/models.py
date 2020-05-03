629d66f17e712596c3394bf31789d3407b5d104a
Jose Tafur <j-osea-nt@hotmail.com>
2020-05-03 12:06:03 -0500

    adding shopping cart view

--- carts/models.py
+++ carts/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from users.models import User
 from products.models import Product
+from django.db.models.signals import pre_save
+import uuid
 # Create your models here.
 class Cart(models.Model):
+    cart_id = models.CharField(max_length=100, null=False, blank=False, unique=True)
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     subtotal = models.DecimalField(default=0.0, max_digits=8, decimal_places=2)
@@ -10,4 +13,12 @@ class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return ''
+        return self.cart_id
+
+def set_cart_id(sender, instance, *args, **kwargs):
+    if not instance.cart_id:
+        # con str lo convierte a cadena el objeto generado por uuid. Uuid genera un identificador
+        instance.cart_id = str(uuid.uuid4())
+
+# registrar el callback, metodo y clase
+pre_save.connect(set_cart_id, sender=Cart)
--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from .models import Cart
 
 # Create your views here.
 def cart(request):
+    # si el usuario está autenticado se obtiene el usuario actual
+    user = request.user if request.user.is_authenticated else None
+    cart_id = request.session.get('cart_id')
+
+    if cart_id:
+        cart = Cart.objects.get(cart_id=cart_id)
+    else:
+        cart = Cart.objects.create(user=user)
+
+    request.session['cart_id'] = cart.cart_id
+
     return render(request, 'carts/cart.html', {
-    
+
     })
