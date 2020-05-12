3a3b02e6fa7039024d9b3eb2c49b1ce7a65d7d16
Jose Tafur <j-osea-nt@hotmail.com>
2020-05-11 21:59:11 -0500

    updating found view

--- carts/views.py
+++ carts/views.py
@@ -1,20 +1,18 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .utils import get_or_create_cart
 from products.models import Product
-
 
 # Create your views here.
 def cart(request):
     cart = get_or_create_cart(request)
-    print(cart)
-    print(cart.products)
+
     return render(request, 'carts/cart.html', {
       'cart': cart
     })
 
 def add(request):
     cart = get_or_create_cart(request)
-    product = Product.objects.get(pk=request.POST.get('product_id'))
+    product = get_object_or_404(Product, pk=request.POST.get('product_id'))
 
     cart.products.add(product)
 
@@ -24,7 +22,7 @@ def add(request):
 
 def remove(request):
     cart = get_or_create_cart(request)
-    product = Product.objects.get(pk=request.POST.get('product_id'))
+    product = get_object_or_404(Product, pk=request.POST.get('product_id'))
 
     cart.products.remove(product)
 
