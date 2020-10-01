5c50fdef0a1ab45573116ca5b1d29c6d6f439659
Jose Tafur <jose.antonio.umeres@gmail.com>
2020-10-01 14:31:09 -0500

    adding redirect action when user is not authenticated

--- orders/views.py
+++ orders/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from carts.utils import get_or_create_cart
 from .models import Order
 from .utils import get_or_create_order
+from django.contrib.auth.decorators import login_required
 
+# decorador que redirige si el usuario no está autenticado
+@login_required(login_url='login')
 def order(request):
     cart = get_or_create_cart(request)
     order = get_or_create_order(cart, request)
