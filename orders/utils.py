8c192261592ac8ff3a5670ad8dff62b56779b9c4
Jose Tafur <jose.antonio.umeres@gmail.com>
2020-10-07 11:57:34 -0500

    adding breadcrumb in order template

--- orders/utils.py
+++ orders/utils.py
@@ -1,4 +1,5 @@
 from .models import Order
+from django.urls import reverse
 
 def get_or_create_order(cart, request):
     # con el metodo get si el objeto no existo envia una exception
@@ -12,3 +13,11 @@ def get_or_create_order(cart, request):
         request.session['order_id'] = order.order_id
 
     return order
+
+def breadcrumb(products=True, address=False, payment=False, confirmation=False):
+    return [
+        {'title': 'Productos', 'active': products, 'url': reverse('orders:order')},
+        {'title': 'Dirección', 'active': address, 'url': reverse('orders:order')},
+        {'title': 'Pago', 'active': payment, 'url': reverse('orders:order')},
+        {'title': 'Confirmación', 'active': confirmation, 'url': reverse('orders:order')}
+    ]
--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from carts.utils import get_or_create_cart
 from .models import Order
-from .utils import get_or_create_order
+from .utils import get_or_create_order, breadcrumb
 from django.contrib.auth.decorators import login_required
 
 # decorador que redirige si el usuario no está autenticado
@@ -13,4 +13,5 @@ def order(request):
     return render(request, 'orders/order.html', {
         'cart': cart,
         'order': order,
+        'breadcrumb': breadcrumb()
     })
