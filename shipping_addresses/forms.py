3161718304a4a86962e74e4a2bf181b085851ca1
Jose Tafur <jose.antonio.umeres@gmail.com>
2020-10-15 09:56:49 -0500

    updating address form fields

--- shipping_addresses/forms.py
+++ shipping_addresses/forms.py
@@ -9,3 +9,47 @@ class ShippingAddressForm(ModelForm):
         fields = [
             'line1', 'line2', 'city', 'state', 'country', 'postal_code', 'reference'
         ]
+        # { atributo del label: valor del label }
+        labels = {
+            'line1': 'Calle 1',
+            'line2': 'Calle 2',
+            'city': 'Ciudad',
+            'state': 'Estado',
+            'country': 'País',
+            'postal_code': 'Código postal',
+            'reference': 'Referencias',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # es un diccionario que almacena los inputs del formulario
+        self.fields['line1'].widget.attrs.update({
+            'class': 'form-control'
+        })
+
+        self.fields['line2'].widget.attrs.update({
+            'class': 'form-control'
+        })
+
+        self.fields['city'].widget.attrs.update({
+            'class': 'form-control'
+        })
+
+
+        self.fields['state'].widget.attrs.update({
+            'class': 'form-control'
+        })
+
+        self.fields['country'].widget.attrs.update({
+            'class': 'form-control'
+        })
+
+        self.fields['postal_code'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': '000'
+        })
+
+        self.fields['reference'].widget.attrs.update({
+            'class': 'form-control'
+        })
