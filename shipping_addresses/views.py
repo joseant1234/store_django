8d9fe1c37b462bc220dffbc9fc1fdace8ec08030
Jose Tafur <jose.antonio.umeres@gmail.com>
2020-10-15 10:12:42 -0500

    adding create method in the shipping address's view

--- shipping_addresses/views.py
+++ shipping_addresses/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from .models import ShippingAddress
 from .forms import ShippingAddressForm
+from django.contrib import messages
 
 # Create your views here.
 class ShippingAddressListView(ListView):
@@ -12,7 +13,20 @@ class ShippingAddressListView(ListView):
         return ShippingAddress.objects.filter(user=self.request.user).order_by('-default')
 
 def create(request):
-    form = ShippingAddressForm()
+    # inicializar el formulario con los valores del request con el metodo post
+    form = ShippingAddressForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        # con el commit en false, el metodo save creará una instancia shipping address pero no la va a persistir
+        shipping_address = form.save(commit=False)
+        shipping_address.user = request.user
+        # se asigna default si es q no hay direcciones para el usuario
+        shipping_address.default = not ShippingAddress.objects.filter(user=request.user).exists()
+        # con el usuario asignado se persiste el shipping address
+        shipping_address.save()
+
+        messages.success(request, 'Dirección creada')
+        return redirect('shipping_addresses:shipping_addresses')
 
     return render(request, 'shipping_addresses/create.html', {
         'form': form
