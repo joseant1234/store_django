d67cd7bf17893f1dddda3b18f4a903f2ba151fea
Jose Tafur <jose.antonio.umeres@gmail.com>
2022-01-17 20:36:32 -0500

    Add external link to the mail

--- orders/mails.py
+++ orders/mails.py
@@ -1,16 +1,24 @@
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
-
+from django.urls import reverse
 
 class Mail:
+
+    @staticmethod
+    def get_absolute_url(url):
+        if settings.DEBUG: #En modo desarrollo
+            return 'http://localhost:8000{}'.format(
+                reverse(url)
+            )
 
     @staticmethod
     def send_complete_order(order, user):
         subject = 'Tu pedido ha sido enviado'
         template = get_template('orders/mails/complete.html')
         content = template.render({
-            'user': user
+            'user': user,
+            'next_url': Mail.get_absolute_url('orders:completed')
         })
         message = EmailMultiAlternatives(subject,
                                         'Mensaje importante',
