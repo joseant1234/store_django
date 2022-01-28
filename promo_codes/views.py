b60c9e6e9a2a002873eaaa7de4fdcbb919f0ede9
Jose Tafur <jose.antonio.umeres@gmail.com>
2022-01-27 21:17:12 -0500

    Added code param to validate url

--- promo_codes/views.py
+++ promo_codes/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
 from django.http import JsonResponse
+from django.shortcuts import render
+from promo_codes.models import PromoCode
+
 
 def validate(request):
+    code = request.GET.get('code')
+    promo_code = PromoCode.objects.filter(code=code).first()
+
+    if promo_code is None:
+        return JsonResponse({
+            'status': False
+        }, status=404)
+
     return JsonResponse({
-        'name': 'Test',
-        'job': 'Demos',
-        'courses': [
-            {'title': 'Python'}
-        ]
+        'status': True,
+        'code': promo_code.code,
+        'discount': promo_code.discount,
     })
