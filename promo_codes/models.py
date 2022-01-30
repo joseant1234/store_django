ac2f3f7e8faba342aeaa650624d7a49626a91266
Jose Tafur <jose.antonio.umeres@gmail.com>
2022-01-30 11:00:57 -0500

    Validated promo code

--- promo_codes/models.py
+++ promo_codes/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.db.models.signals import pre_save
 import string
 import random
+from django.utils import timezone
+
+class PromoCodeManager(models.Manager):
+
+    def get_valid(self, code):
+        now = timezone.now()
+        return self.filter(code=code).filter(used=False).filter(valid_from__lte=now).filter(valid_to__gte=now).first()
 
 class PromoCode(models.Model):
     code = models.CharField(max_length=50, unique=True)
@@ -10,6 +17,8 @@ class PromoCode(models.Model):
     valid_to = models.DateTimeField()
     used = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = PromoCodeManager()
 
     def __str__(self):
         return self.code
